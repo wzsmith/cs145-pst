b2e7418a91820532f2fcd9129b7cdfc4b73f171c
Aaron Chien <102495640+achien2024@users.noreply.github.com>
2024-06-06 22:34:08 -0700

    Update bert.py
    - added majority vote

--- bert.py
+++ bert.py
@@ -540,7 +540,23 @@ def train(year=2023, model_name="scibert"):
             
         loss_history.append(dev_loss)
 
+### ADDING MAJORITY VOTE - AARON
 
+def majority_vote(predictions):
+    """
+    Compute the majority vote from a list of predictions.
+
+    Args:
+    predictions (list): List of prediction arrays or lists.
+
+    Returns:
+    numpy.ndarray: Array of majority vote predictions.
+    """
+    predictions = np.array(predictions)
+    majority = np.mean(predictions, axis=0)  # You can also use np.median for majority vote
+    return np.round(majority).astype(int)
+'''
+OLD CODE - -AARON
 def eval_test_papers_bert(year=2023, model_name="scibert"):
     print("model name", model_name)
     data_year_dir = join(settings.DATA_TRACE_DIR, str(year))
@@ -670,8 +686,171 @@ def eval_test_papers_bert(year=2023, model_name="scibert"):
             print("map until now", np.mean(metrics), len(metrics), cur_map)
 
     print("bert average map", np.mean(metrics), len(metrics))
+'''
+
+def eval_test_papers_bert(year=2023, model_name="scibert", use_majority_vote=False):
+   print("model name", model_name)
+   data_year_dir = join(settings.DATA_TRACE_DIR, str(year))
+   papers_test = utils.load_json(data_year_dir, "paper_source_trace_test.json")
+   pids_test = {p["_id"] for p in papers_test}
 
 
+   in_dir = join(settings.DATA_TRACE_DIR, "paper-xml")
+   files = []
+   for f in os.listdir(in_dir):
+       cur_pid = f.split(".")[0]
+       if f.endswith(".xml") and cur_pid in pids_test:
+           files.append(f)
+
+
+   truths = papers_test
+   pid_to_source_titles = dd(list)
+   for paper in tqdm(truths):
+       pid = paper["_id"]
+       for ref in paper["refs_trace"]:
+           pid_to_source_titles[pid].append(ref["title"].lower())
+
+
+   if model_name == "bert":
+       BERT_MODEL = "bert-base-uncased"
+   elif model_name == "scibert":
+       BERT_MODEL = "allenai/scibert_scivocab_uncased"
+   else:
+       raise NotImplementedError
+   tokenizer = AutoTokenizer.from_pretrained(BERT_MODEL)
+
+
+   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+   print("device", device)
+   model = BertForSequenceClassification.from_pretrained(BERT_MODEL, num_labels = 2)
+   model.to(device)
+   model.eval()
+
+
+   BATCH_SIZE = 16
+   metrics = []
+   f_idx = 0
+
+
+   xml_dir = join(settings.DATA_TRACE_DIR, "paper-xml")
+
+
+   for paper in tqdm(papers_test):
+       cur_pid = paper["_id"]
+       file = join(xml_dir, cur_pid + ".tei.xml")
+       f = open(file, encoding='utf-8')
+
+
+       xml = f.read()
+       bs = BeautifulSoup(xml, "xml")
+       f.close()
+
+
+       source_titles = pid_to_source_titles[cur_pid]
+       if len(source_titles) == 0:
+           continue
+
+
+       references = bs.find_all("biblStruct")
+       bid_to_title = {}
+       n_refs = 0
+       for ref in references:
+           if "xml:id" not in ref.attrs:
+               continue
+           bid = ref.attrs["xml:id"]
+           if ref.analytic is None:
+               continue
+           if ref.analytic.title is None:
+               continue
+           bid_to_title[bid] = ref.analytic.title.text.lower()
+           b_idx = int(bid[1:]) + 1
+           if b_idx > n_refs:
+               n_refs = b_idx
+
+
+       bib_to_contexts = utils.find_bib_context(xml)
+       bib_sorted = sorted(bib_to_contexts.keys())
+
+
+       for bib in bib_sorted:
+           cur_bib_idx = int(bib[1:])
+           if cur_bib_idx + 1 > n_refs:
+               n_refs = cur_bib_idx + 1
+      
+       y_true = [0] * n_refs
+       y_score = [0] * n_refs
+
+
+       flag = False
+       for bid in bid_to_title:
+           cur_ref_title = bid_to_title[bid]
+           for label_title in source_titles:
+               if fuzz.ratio(cur_ref_title, label_title) >= 80:
+                   flag = True
+                   b_idx = int(bid[1:])
+                   y_true[b_idx] = 1
+      
+       if not flag:
+           continue
+
+
+       contexts_sorted = [" ".join(bib_to_contexts[bib]) for bib in bib_sorted]
+
+
+       test_features = convert_examples_to_inputs(contexts_sorted, y_score, MAX_SEQ_LENGTH, tokenizer)
+       test_dataloader = get_data_loader(test_features, MAX_SEQ_LENGTH, BATCH_SIZE, shuffle=False)
+
+
+       if use_majority_vote:
+           predicted_scores = []
+           for step, batch in enumerate(test_dataloader):
+               batch = tuple(t.to(device) for t in batch)
+               input_ids, input_mask, segment_ids, label_ids = batch
+
+
+               with torch.no_grad():
+                   r = model(input_ids, attention_mask=input_mask,
+                                               token_type_ids=segment_ids, labels=label_ids)
+                   tmp_eval_loss = r[0]
+                   logits = r[1]
+
+
+               cur_pred_scores = logits[:, 1].to('cpu').numpy()
+               predicted_scores.extend(cur_pred_scores)
+          
+           for ii in range(len(predicted_scores)):
+               bib_idx = int(bib_sorted[ii][1:])
+               y_score[bib_idx] = predicted_scores[ii]
+
+
+       else:
+           predicted_scores = []
+           for step, batch in enumerate(test_dataloader):
+               batch = tuple(t.to(device) for t in batch)
+               input_ids, input_mask, segment_ids, label_ids = batch
+
+
+               with torch.no_grad():
+                   r = model(input_ids, attention_mask=input_mask,
+                                               token_type_ids=segment_ids, labels=label_ids)
+                   tmp_eval_loss = r[0]
+                   logits = r[1]
+
+
+               cur_pred_scores = logits[:, 1].to('cpu').numpy()
+               predicted_scores.extend(cur_pred_scores)
+          
+           for ii in range(len(predicted_scores)):
+               bib_idx = int(bib_sorted[ii][1:])
+               y_score[bib_idx] = predicted_scores[ii]
+
+
+       cur_map = average_precision_score(y_true, y_score)
+       metrics.append(cur_map)
+       f_idx += 1
+       if f_idx % 20 == 0:
+           print("map until now", np.mean(metrics), len(metrics), cur_map
+                 
 def gen_kddcup_valid_submission_bert(model_name="scibert"):
     print("model name", model_name)
     data_dir = join(settings.DATA_TRACE_DIR, "PST")
